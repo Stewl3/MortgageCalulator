daa8096da236c89111ebb205dad96a2b178cb154
Stewl3 <stevecraft2014@outlook.com>
2023-03-03 23:14:48 -0500

    Brought the main method to the top. tested separate Main.Java file to run the program.

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import com.stewie.MortgageCalc;
+
 public class Main {
     public static void main(String[] args) {
+        int principle = (int)MortgageCalc.readNumber("Enter a number between 1,000 and 1,000,000: ", 1_000, 1_000_000);
+        float annualInterest = (float)MortgageCalc.readNumber("Enter a number between 1 and 30: ", 1, 30);
+        byte years = (byte)MortgageCalc.readNumber("Enter a numbe 1 and 30: ", 1, 30);
+
+        MortgageCalc.printMortgage(principle, annualInterest, years);
+        MortgageCalc.printPaymentSchedule(principle, annualInterest, years);
 
     }
 }
